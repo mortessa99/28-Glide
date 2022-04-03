16474343d50467589290a1b2ca7dab74b61847b3
mortessa99 <smortezajahan@gmail.com>
2022-04-03 05:07:43 +0430

    final

--- app/src/main/java/com/example/a28_glide/MainActivity.kt
+++ app/src/main/java/com/example/a28_glide/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.a28_glide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            loadImage()
+        }
+    }
+
+    private fun loadImage() {
+        val url = "https://picsum.photos/500"
+        Glide
+            .with(this)
+            .load(url)
+            .centerCrop()
+            //.placeholder(R.drawable.ic_launcher_foreground)
+            .into(imageView)
     }
 }
